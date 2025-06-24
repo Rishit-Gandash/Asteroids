cba692505b1aaf0b7ec3f748785dd142b758678b
splitzerr <rishikgandash@gmail.com>
2025-06-24 16:23:40 +0530

    Made the player!

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 from constants import * 
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        screen.fill(00000)
+        
+        
+        screen.fill((00,00,00))
+        player.draw(screen)
         pygame.display.flip()
 
-
-    print("Starting Asteroids!")
-
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
