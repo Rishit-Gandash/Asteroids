40fe87ee70b3eeb3c11b53e3607f917b0b42a1e2
splitzerr <rishikgandash@gmail.com>
2025-06-24 16:49:52 +0530

    Made the player rotate!

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
+
+        updatable.update(dt)
+
         screen.fill((00,00,00))
-        player.draw(screen)
+
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 
